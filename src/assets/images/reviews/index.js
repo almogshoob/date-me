a77f1125dd1336269dfa7f97f7aa2309b219cd58
Almog Shoob <almogshoob1@gmail.com>
2025-02-15 20:38:20 +0200

    init

--- src/assets/images/reviews/index.js
+++
@@ -1,4 +0,0 @@
-export { default as imgEden } from "./eden.jpg";
-export { default as imgElla } from "./ella.jpg";
-export { default as imgYoel } from "./yoel.jpg";
-export { default as imgStopel } from "./stopel.jpg";
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./WhoAmI/WhoAmI";
-export * from "./WhoIsItFor/WhoIsItFor";
-export * from "./Reviews/Reviews";
-export * from "./Contact/Contact";
